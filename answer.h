83dac85e808ea49143724bdccda10a33d40f1bec
Dananjaya Ramanayake <dananjaya86@yahoo.com>
2022-08-11 22:11:45 +0200

    add new functionality and associated test

--- answer.h
+++ answer.h
@@ -1,1 +1,2 @@
 int return_answer(void);
+int return_answer_2_0(void);
--- test_main.c
+++ test_main.c
@@ -1,8 +1,27 @@
 #include "answer.h"
 
+int test_answer(void);
+int test_dev_answer(void);
+
+int test_answer(void) {
+    if (return_answer() == 42) {
+        return 0;
+    } else {
+        return -1;
+    }
+}
+
+int test_dev_answer() {
+    if(return_answer_2_0() == 84) {
+        return 0;
+    } else {
+        return -1;
+    }
+}
+
 int main() {
   
-  if (return_answer() == 42) {
+  if (!test_answer() && !test_dev_answer()) {
 
     return 0;
 
